c9f80bec0e2429d5a92425488200ea397c87ce1f
Simanov <simanovdei@gmail.com>
2014-04-29 22:28:12 +0300

    qwe

---
+++ template/users/js/main.js
@@ -0,0 +1,11 @@
+
+
+// При клике - появление блока выбора языка.
+var i = $('.swt').click(function(e) {
+	var s = $('.switch');
+s.slideToggle(20);
+});
+var i = $('.ml').click(function(e) {
+	var s = $('.switch');
+s.slideToggle(20);
+});
